69d0eee8879a39e6565ff5f11f4325e9c6e20ddb
Pratham <109359719+pratham15541@users.noreply.github.com>
2023-10-07 20:20:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pairwise swap elements of a linked list/pairwise-swap-elements-of-a-linked-list.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+typedef struct Node node;
+class Solution
+{
+    public:
+     Node* pairWiseSwap(struct Node* head) {
+        if (!head || !head->next) return head;
+        
+        node* tempTwo = head->next;
+        head->next = head->next->next;
+        tempTwo->next = head;
+        head = tempTwo;
+        
+        node* p = head->next;
+        node* q = p ? p->next : nullptr;
+        node* r = q ? q->next : nullptr;
+        
+        while (p && q && r) {
+            p->next = q->next;
+            q->next = r->next;
+            r->next = q;
+            p = q;
+            q = p ? p->next : nullptr;
+            r = q ? q->next : nullptr;
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
+}
+
+// } Driver Code Ends
