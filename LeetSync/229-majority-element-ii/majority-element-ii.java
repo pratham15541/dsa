e5876b68f5480d73301928281fe3ae5a4cf1e917
Pratham Parikh <prathamparikh94@gmail.com>
2025-06-14 10:45:31 +0530

    Time: 2 ms (99.55%) | Memory: 50 MB (57.82%) - LeetSync

--- LeetSync/229-majority-element-ii/majority-element-ii.java
+++ LeetSync/229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        int count1 = 0, count2 = 0; 
+        int candidate1 = 0, candidate2 = 0; 
 
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0) + 1);
-        }
 
-        List<Integer> arr = new ArrayList<>();
-        int thresHold = nums.length/3;
-
-        for(Map.Entry<Integer,Integer> e : map.entrySet()){
-            int ele = e.getKey();
-            int val = e.getValue();
-
-            if(val > thresHold){
-                arr.add(ele);
+        for (int i = 0; i < nums.length; i++) {
+          
+            if (count1 == 0 && nums[i] != candidate2) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } 
+            
+            else if (count2 == 0 && nums[i] != candidate1) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } 
+          
+            else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } 
+            
+            else {
+                count1--;
+                count2--;
             }
         }
-    return arr;
-      
+
+        List<Integer> result = new ArrayList<>();
+        int threshold = nums.length / 3; 
+
+        
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+
+       
+        if (count1 > threshold) {
+            result.add(candidate1);
+        }
+        if (count2 > threshold) {
+            result.add(candidate2);
+        }
+
+        return result;
     }
 }
