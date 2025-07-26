179be6c7696eebbde5633f1651204f9e341b88ba
Pratham Parikh <prathamparikh94@gmail.com>
2025-07-26 09:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 83.6 MB (88.04%) - LeetSync

--- LeetSync/2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ LeetSync/2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,22 +1,36 @@
 class Solution {
-    public int[] findPeakGrid(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int current = mat[i][j];
 
-                int up = (i > 0) ? mat[i - 1][j] : Integer.MIN_VALUE;
-                int down = (i < m - 1) ? mat[i + 1][j] : Integer.MIN_VALUE;
-                int left = (j > 0) ? mat[i][j - 1] : Integer.MIN_VALUE;
-                int right = (j < n - 1) ? mat[i][j + 1] : Integer.MIN_VALUE;
-
-                if (current > up && current > down && current > left && current > right) {
-                    return new int[] { i, j };
-                }
+      public int maxEl(int[][] mat, int n, int m, int col) {
+        int maxi = -1;
+        int ind = -1;
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > maxi) {
+                maxi = mat[i][col];
+                ind = i;
             }
         }
+        return ind;
+    }
 
-        return new int[] { -1, -1 };
+    public int[] findPeakGrid(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0;
+        int high = m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int row = maxEl(mat, n, m, mid);
+            int left = mid - 1 >= 0 ? mat[row][mid - 1] : -1;
+            int right = mid + 1 < m ? mat[row][mid + 1] : -1;
+
+            if (mat[row][mid] > left && mat[row][mid] > right)
+                return new int[]{row, mid};
+            else if (mat[row][mid] < left)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return new int[]{-1, -1};
     }
 }
