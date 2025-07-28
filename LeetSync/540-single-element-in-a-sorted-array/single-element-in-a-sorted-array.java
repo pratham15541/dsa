0b1abed7ebb50e51e5529f2b401d33ae489533a2
Pratham Parikh <prathamparikh94@gmail.com>
2025-07-28 08:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (52.53%) - LeetSync

--- LeetSync/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ LeetSync/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int l = 0, h = nums.length - 1;
+       int l = 0, h = nums.length - 1;
         while (l < h) {
             int mid = l + (h - l) / 2;
             if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || mid % 2 == 1 && nums[mid] == nums[mid - 1]) {
@@ -9,6 +9,6 @@ class Solution {
                 h = mid;
             }
         }
-        return nums[l];
+        return nums[l]; 
     }
 }
