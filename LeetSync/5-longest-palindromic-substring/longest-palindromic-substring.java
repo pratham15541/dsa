e5b1914f8a55c39bc4fc890350440ef4138f02b5
Pratham Parikh <prathamparikh94@gmail.com>
2025-08-16 10:04:42 +0530

    Time: 2552 ms (5.05%) | Memory: 45.6 MB (21.55%) - LeetSync

---
+++ LeetSync/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1)
+            return "";
+
+        String longest = "";
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                String sub = s.substring(i, j);
+                if (isPalindrome(sub) && sub.length() > longest.length()) {
+                    longest = sub;
+                }
+            }
+        }
+        return longest;
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
