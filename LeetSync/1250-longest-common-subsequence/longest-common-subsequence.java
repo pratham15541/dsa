65ce68c78404a6867d463453a1f0b128cfe14f6f
Pratham Parikh <prathamparikh94@gmail.com>
2025-08-27 11:03:43 +0530

    Time: 27 ms (20.74%) | Memory: 50.6 MB (83.26%) - LeetSync

--- LeetSync/1250-longest-common-subsequence/longest-common-subsequence.java
+++ LeetSync/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,18 +1,33 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        Integer[][] dp = new Integer[text1.length()][text2.length()];
-        return lcs(text1,text2,0,0,dp);
-    }
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
 
-    private int lcs(String s1, String s2, int i, int j , Integer[][] dp){
-        if(i>= s1.length() || j >= s2.length()) return 0;
-        if(dp[i][j] != null) return dp[i][j];
-        if (s1.charAt(i) == s2.charAt(j)) {
-            dp[i][j] = 1 + lcs(s1, s2, i + 1, j + 1, dp);
-        } else {
-            dp[i][j] = Math.max(lcs(s1, s2, i + 1, j, dp), lcs(s1, s2, i, j + 1, dp));
+            for (int i = n-1; i >= 0; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j))
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
         }
 
-        return dp[i][j];
+        StringBuilder sb= new StringBuilder();
+         int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (text1.charAt(i) == text2.charAt(j)) {
+                sb.append(text1.charAt(i));
+                i++; j++;
+            } else if (dp[i+1][j] >= dp[i][j+1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        System.out.println("LCS Length: " + dp[0][0]);
+        System.out.println("LCS String: " + sb.toString());
+        return dp[0][0];
     }
 }
