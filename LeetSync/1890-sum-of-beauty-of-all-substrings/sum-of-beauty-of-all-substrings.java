998082dcc068bc3cec399e2202c6eecd96ee3b17
Pratham Parikh <prathamparikh94@gmail.com>
2025-09-17 07:55:20 +0530

    Time: 1248 ms (5.05%) | Memory: 45.5 MB (6.22%) - LeetSync

---
+++ LeetSync/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int beautySum(String s) {
+       int n = s.length();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            Map<Character, Integer> mp = new HashMap<>();
+            for (int j = i; j < n; j++) {
+                char c = s.charAt(j);
+                mp.put(c, mp.getOrDefault(c, 0) + 1);
+
+                int maxFreq = Collections.max(mp.values());
+                int minFreq = Collections.min(mp.values());
+
+                sum += (maxFreq - minFreq);
+            }
+        }
+        return sum;
+    }
+}
