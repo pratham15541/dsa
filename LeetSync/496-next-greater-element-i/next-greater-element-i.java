e0aa742404b695c4f57afcd59c9990c4de29af85
Pratham Parikh <prathamparikh94@gmail.com>
2025-10-25 15:04:56 +0530

    Time: 3 ms (88.31%) | Memory: 43.5 MB (77.95%) - LeetSync

--- LeetSync/496-next-greater-element-i/next-greater-element-i.java
+++ LeetSync/496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
         int[] ans = new int[nums1.length];
-
         for (int i = 0; i < nums1.length; i++) {
-            int target = nums1[i];
-            int nextGreater = -1; 
-
- 
-            int targetIndexInNums2 = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == target) {
-                    targetIndexInNums2 = j;
-                    break;
-                }
-            }
-
-            
-            if (targetIndexInNums2 != -1) {
-                for (int k = targetIndexInNums2 + 1; k < nums2.length; k++) {
-                    if (nums2[k] > target) {
-                        nextGreater = nums2[k];
-                        break; 
-                    }
-                }
-            }
-            ans[i] = nextGreater;
+            ans[i] = nextGreater.getOrDefault(nums1[i], -1);
         }
         return ans;
     }
